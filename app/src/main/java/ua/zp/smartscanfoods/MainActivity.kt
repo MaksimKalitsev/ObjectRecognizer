3be8ce5aa4e42f4001fa6a190fb8efc8e226eba1
Maksim.Kalitsev <maksim.kalitsev@ukr.net>
2023-07-07 12:59:53 +0300

    create OnboardingScreen

--- app/src/main/java/ua/zp/smartscanfoods/MainActivity.kt
+++ app/src/main/java/ua/zp/smartscanfoods/MainActivity.kt
@@ -3,13 +3,25 @@ package ua.zp.smartscanfoods
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ua.zp.smartscanfoods.ui.theme.SmartScanFoodsTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    OnboardingScreen(onContinueClicked = { /*TODO*/ })
                 }
             }
         }
@@ -30,17 +42,56 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun OnboardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize().padding(top = 50.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "WELCOME!")
+        Row(
+            modifier = modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(
+                modifier = modifier
+                    .padding(top = 24.dp, end = 12.dp)
+                    .size(100.dp)
+                    .clip(CircleShape),
+                onClick = onContinueClicked
+            ) {
+                Text(
+                    text = "Take photo",
+                    textAlign = TextAlign.Center,
+                    fontSize = 18.sp
+                )
+            }
+            Button(
+                modifier = modifier
+                    .padding(top = 24.dp, start = 12.dp)
+                    .size(100.dp)
+                    .clip(CircleShape),
+                onClick = onContinueClicked
+            ) {
+                Text(
+                    text = "Load image",
+                    textAlign = TextAlign.Center,
+                    fontSize = 18.sp
+                )
+            }
+        }
+
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
-fun GreetingPreview() {
+fun OnboardingPreview() {
     SmartScanFoodsTheme {
-        Greeting("Android")
+        OnboardingScreen(onContinueClicked = {})
     }
 }
